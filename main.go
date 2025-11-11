881a037749c1f3c47581168af3970ac6691d52cd
hao <qin.hao9@gmail.com>
2025-11-11 05:03:39 -0500

    add logic to allow users to update their own email and password

--- main.go
+++ main.go
@@ -76,6 +76,7 @@ func main() {
 	mux.HandleFunc("POST /api/revoke", apiCfg.handlerRevoke)
 
 	mux.HandleFunc("POST /api/users", apiCfg.handlerUsersCreate)
+	mux.HandleFunc("PUT /api/users", apiCfg.handlerUsersUpdate)
 
 	mux.HandleFunc("GET /api/chirps", apiCfg.handlerChirpsGet)
 	mux.HandleFunc("GET /api/chirps/{chirpID}", apiCfg.handlerChirpGet)
